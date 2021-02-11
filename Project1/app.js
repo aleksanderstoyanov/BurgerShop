6c1200ce63cae47df47751e654f31f2ac29b2f8f
Alexander Stoyanov <63510557+aleksanderstoyanov@users.noreply.github.com>
2021-02-11 23:12:03 +0200

    Adding login authentication

--- Project1/app.js
+++ Project1/app.js
@@ -3,7 +3,7 @@ import { loadIntro } from "../views/intro.js";
 import { loadBurgers } from "../views/menu/burgers.js";
 import { loadDrinks } from "../views/menu/drinks.js";
 import { loadMenu } from "../views/menu/menu.js";
-import { loadLogin } from "../views/users/login.js";
+import { loadLogin, postLogin } from "../views/users/login.js";
 import { loadRegister, postRegister } from "../views/users/register.js";
 import {loadProfile} from '../views/users/profile.js';
 import { logOutOfUser } from "../views/users/logout.js";
@@ -12,7 +12,6 @@ import { logOutOfUser } from "../views/users/logout.js";
 
 const app = Sammy('#root', function (context) {
     this.use('Handlebars', 'hbs')
-
 
     this.get('#/intro',loadIntro);
     this.get('#/home',loadHome);
@@ -23,8 +22,8 @@ const app = Sammy('#root', function (context) {
     this.get('#/drinks',loadDrinks);
     this.get('#/profile',loadProfile);
     this.get('#/logOut',logOutOfUser);
- 
-  this.post('#/register',postRegister);
+    this.post('#/register',postRegister);
+    this.post('#/login',postLogin);
 });
 
 app.run('#/intro');
--- views/users/login.js
+++ views/users/login.js
@@ -3,3 +3,8 @@ import partials from '../partials.js';
 export function loadLogin(context){
     context.loadPartials(partials).partial('./templates/login.hbs');
 }
+
+export function postLogin(context){
+    const{username,password}=context.params;
+    console.log(username);
+}
